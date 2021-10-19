bdfef11ff3bd0a45e3388007ca5ee009d851119a
Kenny Yepez <kfyepez@espol.edu.ec>
2021-10-18 20:57:38 -0500

    Insurance Class

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Insurance.java
@@ -0,0 +1,42 @@
+package ec.edu.espol.workshops;
+
+public class Insurance {
+
+	private int basePremium = 500;
+	private int ageLimit = 80;
+	protected Customer customer;
+	protected int premium;
+	
+	public Insurance(Customer customer) {
+		this.customer=customer;
+		this.premium=basePremium;
+			
+	}
+	
+	public int calculateInsure() {
+		if(customer.getAge()>80 || customer.getDriverLicense()==false) {
+			this.premium=-1;
+			return premium;
+		}
+		if(customer.getAge()>45 && customer.getAge()<65) {
+			this.premium-=100;
+		}
+		if(customer.getSex()=="M" && customer.getisMarried()==false && customer.getAge()<25) {
+			this.premium+=1500;
+		}
+		if(customer.getSex()=="F" || customer.getisMarried()==true) {
+			this.premium-=200;
+		}
+		return premium;
+	}
+	
+	public int getPremium() {
+		return premium;
+	}
+
+
+
+	
+
+
+}
