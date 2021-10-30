2334a7361463941faf0cbe81ad2fdbdac597faeb
KevinB1500 <kdbautis@espol.edu.ec>
2021-10-29 20:47:55 -0500

    Minimun corrections

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -9,7 +9,6 @@ public class CarInsurance {
 	*
 	* 
 	*/
-	static final Logger log = Logger.getLogger(CarInsurance.class.getName());
 	
 	public static void main(String[] args) {
     	
@@ -19,36 +18,48 @@ public class CarInsurance {
 		String sex;
 		String licence;
 		String married;
+		boolean licenceb;
+		boolean marriedb;
 		
 		
-		log.fine("Enter your age:");
+		System.out.println("Enter your age:");
     	age = input.nextInt();
     	input.nextLine();
     	
     	do {    		
-    		log.fine("Enter your gender (M/F):");
+    		System.out.println("Enter your gender (M/F):");
     		sex =  input.nextLine().strip();
     	} while (!(sex.equals("M") || sex.equals("F"))); 
     	
     	do {
-    		log.fine("Do you have driver licence? (Y/N):");
-    		licence = input.nextLine().strip();    		
+    		System.out.println("Do you have driver licence? (Y/N):");
+    		licence = input.nextLine();    		
     	} while (!(licence.equals("Y") || !licence.equals("N")));
+    	if(licence.equals("Y")) {
+    		licenceb = true;
+    	}else {
+    		licenceb = false;
+    	}
     	 
     	do {
-    		log.fine("Are you married? (Y/N):");
-    		married = input.nextLine().strip();
+    		System.out.println("Are you married? (Y/N):");
+    		married = input.nextLine();
     	}
     	while (!(married.equals("Y") || married.equals("N")));
+    	if(married.equals("Y")) {
+    		marriedb = true;
+    	}else {
+    		marriedb = false;
+    	}
     	
     	input.close();
     	
-		Customer client = new Customer(age, licence.equals("Y"),sex, married.equals("Y"));
+		Customer client = new Customer(age, licenceb,sex, marriedb);
     	Insurance insurance = new Insurance(client);
 		insurance.calculateInsure();
 		
 
-		log.fine("You have to pay: $"+insurance.getPremium());
+		System.out.println("You have to pay: $"+insurance.getPremium());
 
     	
     }
